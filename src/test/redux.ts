895bb5cb99590d2c67e2c2d3d615dda03d3d77fd
pangxiao.px <pangxiao.px@alibaba-inc.com>
2019-10-25 14:44:57 +0800

    feat: 增加 npm scripts

---
+++ src/test/redux.ts
@@ -0,0 +1,18 @@
+import { rapper } from '../index';
+import { resolve } from 'path';
+
+rapper({
+  type: 'redux',
+  /** rap项目id */
+  projectId: 3564,
+  /** 输出文件的目录，默认是 ./model */
+  rapperPath: resolve(process.cwd(), './test_data/model'),
+  /** rap地址，默认是 http://rap2api.taobao.org */
+  rapUrl: 'https://rap2api.alibaba-inc.com',
+});
+// .then(() => {
+//     console.log('rapper:generate model success')
+// })
+// .catch(err => {
+//     console.log('rapper:generate model failed', err)
+// })
