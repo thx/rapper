406fa9482d305cbd57db3d4372dc0901ea98302b
pangxiao.px <pangxiao.px@alibaba-inc.com>
2019-12-02 11:59:00 +0800

    feat: 优化提示

--- test/src/models/rapper/index.ts
+++ test/src/models/rapper/index.ts
@@ -3,12 +3,38 @@
 
 /**
  * 本文件由 Rapper 同步 Rap 平台接口，自动生成，请勿修改
- * Rap仓库 地址: https://rap2.alibaba-inc.com/repository/editor?id=3601
+ * Rap仓库 地址: https://rap2.taobao.org/repository/editor?id=237514
  */
 
 import { createFetch, IModels } from './request';
+
+import {
+  useResponse,
+  useAllResponse,
+  clearResponseCache,
+  rapperActions,
+  rapperSelector,
+} from './redux';
+import { rapperReducers, rapperEnhancer } from './lib';
+import { IResponseTypes } from './request';
 
 const fetch = createFetch({});
 
 export { fetch, createFetch };
 export type Models = IModels;
+
+export {
+  /** 以Hooks的方式使用请求响应数据 */
+  useResponse,
+  /** 使用请求响应数据（包含缓存） */
+  useAllResponse,
+  /** 清除此接口的缓存 */
+  clearResponseCache,
+  rapperSelector,
+  rapperActions,
+  rapperReducers,
+  rapperEnhancer,
+};
+
+/** 响应类型 */
+export type ResponseTypes = IResponseTypes;
