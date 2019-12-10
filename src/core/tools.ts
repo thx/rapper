9ce8d994442ec380062ebe1ac508d4a6bd2157d3
pangxiao.px <pangxiao.px@alibaba-inc.com>
2019-12-10 12:14:32 +0800

    fix: 更新lodash

--- src/core/tools.ts
+++ src/core/tools.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 import axios from 'axios';
-import _ = require('lodash');
+import * as _ from 'lodash';
 import { IModules, ICollaborator, Interface, Intf, IUrlMapper } from '../types';
 
 function updateURLParameter(url: string, param: string, paramVal: string) {
