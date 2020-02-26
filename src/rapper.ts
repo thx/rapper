1588509e1d3bd9de3f9ec0b6822f8901fb52eb5d
pangxiao.px <pangxiao.px@alibaba-inc.com>
2020-02-26 17:34:30 +0800

    fix: 升级

--- src/rapper.ts
+++ src/rapper.ts
@@ -62,9 +62,9 @@ export default async function({
       spinner.warn(chalk.yellow('rapper 升级提示: '));
       console.log(`  当前版本: ${chalk.grey(packageJson.version)}`);
       console.log(`  最新版本: ${chalk.cyan(newVersion)}`);
-      console.log(
-        `  运行 ${chalk.green(`npm i -D ${packageJson.name}@latest && npm run rapper`)} 即可升级`,
-      );
+      // console.log(
+      //   `  运行 ${chalk.green(`npm i -D ${packageJson.name}@latest && npm run rapper`)} 即可升级`,
+      // );
     } else {
       spinner.succeed(chalk.grey('rapper: 当前是最新版'));
     }
