89c7e1ad0b6632bb825181b7763a1a01dbcafb91
chibing.fy <chibing.fy@alibaba-inc.com>
2020-02-28 18:37:31 +0800

    fix: update runtime copy

--- script/move-runtime.js
+++ script/move-runtime.js
@@ -1,8 +1,14 @@
-const mv = require('mv');
+const fs = require('fs-extra');
 const path = require('path');
 const cwd = process.cwd();
-mv(path.join(cwd, 'dist/runtime'), path.join(cwd, '/runtime'), function(err) {
-  if (err) {
+const src = path.join(cwd, 'dist/runtime');
+const dist = path.join(cwd, '/runtime');
+
+(async function() {
+  try {
+    await fs.remove(dist);
+    await fs.copy(src, dist);
+  } catch (err) {
     console.log(err);
   }
-});
+})();
